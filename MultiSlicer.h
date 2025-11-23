5b000e38a183259417ebb43fe08c8c3a4445d198
rebuildup <rebuild.up.up@gmail.com>
2025-11-23 14:46:32 +0900

    feat: MultiSlicer.hにAfter Effectsプラグインのヘッダーボイラープレート、パラメータ定義、ユーティリティマクロ、および`EffectMain`関数の宣言を追加しました。

--- MultiSlicer.h
+++ MultiSlicer.h
@@ -1,4 +1,100 @@
-    ANGLE_DISK_ID = 1,
+/*******************************************************************/
+/*                                                                 */
+/*                      ADOBE CONFIDENTIAL                         */
+/*                   _ _ _ _ _ _ _ _ _ _ _ _ _                     */
+/*                                                                 */
+/* Copyright 2007-2023 Adobe Inc.                                  */
+/* All Rights Reserved.                                            */
+/*                                                                 */
+/* NOTICE:  All information contained herein is, and remains the   */
+/* property of Adobe Inc. and its suppliers, if                    */
+/* any.  The intellectual and technical concepts contained         */
+/* herein are proprietary to Adobe Inc. and its                    */
+/* suppliers and may be covered by U.S. and Foreign Patents,       */
+/* patents in process, and are protected by trade secret or        */
+/* copyright law.  Dissemination of this information or            */
+/* reproduction of this material is strictly forbidden unless      */
+/* prior written permission is obtained from Adobe Inc.            */
+/* Incorporated.                                                   */
+/*                                                                 */
+/*******************************************************************/
+
+#pragma once
+
+#ifndef MULTISLICER_H
+#define MULTISLICER_H
+
+#define PF_DEEP_COLOR_AWARE 1
+
+#include "AEConfig.h"
+
+#ifdef AE_OS_WIN
+    typedef unsigned short PixelType;
+    #include <Windows.h>
+#endif
+
+#include "entry.h"
+#include "AE_Effect.h"
+#include "AE_EffectCB.h"
+#include "AE_Macros.h"
+#include "Param_Utils.h"
+#include "AE_EffectCBSuites.h"
+#include "String_Utils.h"
+#include "AE_GeneralPlug.h"
+#include "AEFX_ChannelDepthTpl.h"
+#include "AEGP_SuiteHandler.h"
+
+#include "MultiSlicer_Strings.h"
+
+/* Versioning information */
+#define	MAJOR_VERSION	1
+#define	MINOR_VERSION	0
+#define	BUG_VERSION		0
+#define	STAGE_VERSION	PF_Stage_DEVELOP
+#define	BUILD_VERSION	1
+
+#define MULTISLICER_WIDTH_MIN         0
+#define MULTISLICER_WIDTH_MAX         100
+#define MULTISLICER_WIDTH_DFLT        100
+
+#define MULTISLICER_SLICES_MIN        2
+#define MULTISLICER_SLICES_MAX        100
+#define MULTISLICER_SLICES_DFLT       10
+
+#define MULTISLICER_SEED_MIN          0
+#define MULTISLICER_SEED_MAX          10000
+#define MULTISLICER_SEED_DFLT         1234
+
+// Macro for math operations
+#define CLAMP(x, min, max) ((x) < (min) ? (min) : ((x) > (max) ? (max) : (x)))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+enum {
+    MULTISLICER_INPUT = 0,
+    MULTISLICER_ANGLE,
+    MULTISLICER_SHIFT,
+    MULTISLICER_WIDTH,
+    MULTISLICER_SLICES,
+    MULTISLICER_SEED,
+    MULTISLICER_NUM_PARAMS
+};
+
+enum {
+    ANGLE_DISK_ID = 1
+};
+
+extern "C" {
+    DllExport
+    PF_Err
+    EffectMain(
+        PF_Cmd          cmd,
+        PF_InData       *in_data,
+        PF_OutData      *out_data,
+        PF_ParamDef     *params[],
+        PF_LayerDef     *output,
+        void            *extra);
 }
 
-#endif // MultiSlicer_H
+#endif // MULTISLICER_H
